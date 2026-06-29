ee95652830c61a284de111ff91478d3a9b43d502
Jhonattan <jhonattan10alves@gmail.com>
2026-06-29 12:38:11 -0300

    master scripts

---
+++ Digisac/Tv/script.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Contar Consultoria Digisac Monitor Chats + Fila v3.0
+// @namespace    contar.digisac.monitor
+// @version      3.0.0
+// @description  Alerta para chats e fila - Versão usuário
+// @author       Tiago Debossan <tiagodebossan@contarconsultoria.com>
+// @author       Gabriel Silveira <gabrielsilveira@contarconsultoria.com>
+// @match        https://contarconsultoria.digisac.io/*
+// @grant        GM_notification
+// @require      https://raw.githubusercontent.com/Contar-Consultoria/TamperMonkeyScripts/refs/heads/master/Digisac/Tv/DigisacMonitorChatsFila.js
+// @run-at       document-start
+// ==/UserScript==
+
+// Script carregado do GitHub - NÃO EDITE
